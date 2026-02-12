0f650920d73a55b8a5a704e7fd3047c8d3077abb
247544765+nami-commits@users.noreply.github.com <247544765+nami-commits@users.noreply.github.com>
2026-02-12 19:08:29 +0900

    完了処理・削除処理の実装

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\TaskRegisterController;
 use App\Http\Controllers\TaskListController;
-
+use App\Http\Controllers\TaskEditController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -20,15 +20,19 @@ Route::get('/task/register', [TaskRegisterController::class, 'index'])->name('sh
 // タスク登録画面  登録処理
 Route::post('/task/register', [TaskRegisterController::class, 'register'])->name('task_register');
 
-// タスク一覧画面  登録処理
+// タスク一覧画面  表示処理
 Route::get('/task/list', [TaskListController::class, 'index'])->name('show_task_list');
 
 // タスク編集画面  表示処理
-Route::get('/task/edit/{task_id}', [TaskListController::class, 'index'])->name('show_task_edit');
+Route::get('/task/edit/{task_id}', [TaskEditController::class, 'index'])->name('show_task_edit');
 
 // タスク編集画面  更新処理
-Route::post('/task/edit/{task_id}', [TaskListController::class, 'edit'])->name('task_edit');
+Route::post('/task/edit/{task_id}', [TaskEditController::class, 'edit'])->name('task_edit');
 
+Route::post('/task/finish/{task_id}', [TaskListController::class, 'finish'])->name('task_finish');
+
+// タスク一覧画面  タスク削除処理
+Route::post('/task/delete/{task_id}', [TaskListController::class, 'delete'])->name('task_delete');
 
 
 
